f0ee1ce63d7d327cd1523501a663a58e8b93e3f8
Stif <stifskere@gmail.com>
2022-06-28 19:40:22 +0200

    boilerplate

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace FPB;
+
+public static class Bot
+{
+    public static Task Main()
+    {
+        return MainAsync("");
+    }
+
+    public static readonly DiscordSocketClient Client = new(new DiscordSocketConfig {GatewayIntents = GatewayIntents.All});
+
+    private static async Task MainAsync(string token)
+    {
+        //events
+        
+        await Client.LoginAsync(TokenType.Bot, token);
+        await Client.StartAsync();
+        await Task.Delay(-1);
+    }
+}
