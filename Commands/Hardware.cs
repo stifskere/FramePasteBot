63d9211db5499ea40665847226fd6cd781b0d412
Stif <stifskere@gmail.com>
2022-07-18 13:18:09 +0200

    hardware base

---
+++ Commands/Hardware.cs
@@ -0,0 +1,39 @@
+﻿using Discord.Interactions;
+
+namespace FPB.Commands;
+
+[Group("hardware", "Hardware group")]
+public class Hardware : InteractionModuleBase<SocketInteractionContext>
+{
+    [Group("cpu", "Hardware cpu group")]
+    public class cpus : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("list", "Displays a list from a keyword")]
+        public async Task ListAsync()
+        {
+        
+        }
+
+        [SlashCommand("find", "Displays the exact hardware part if found")]
+        public async Task FindAsync()
+        {
+        
+        }
+    }
+
+    [Group("gpu", "Hardware gpu group")]
+    public class gpus : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("list", "Displays a list from a keyword")]
+        public async Task ListAsync()
+        {
+        
+        }
+
+        [SlashCommand("find", "Displays the exact hardware part if found")]
+        public async Task FindAsync()
+        {
+        
+        }
+    }
+}
